ef8a9ffc7d767bf144657f817d25fa8aaa0aa329
Kevin Hurts <kevinhurts@gmail.com>
2020-09-07 12:31:34 +0200

    Add transpiling

--- index.js
+++
@@ -1,6 +0,0 @@
-import useFormState from './src/useFormState'
-import createFormValidation from './src/createFormValidation'
-
-export { createFormValidation }
-
-export default useFormState
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import useFormState from './useFormState'
+import createFormValidation from './createFormValidation'
+
+export { createFormValidation }
+
+export default useFormState
